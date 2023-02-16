f4bfe85fa69d03a97787ee8bfdd60643330eb33e
TEJA <suryateja1401@gmail.com>
2023-02-16 07:20:15 +0530

    conditions

--- print.sh
+++ print.sh
@@ -2,5 +2,8 @@ echo hello world
 #colour print syntax
 echo -e "\e[COLm MESSAGE  \e[0m"
 #ex echo -e "\e[31m hello  \e[0m"
-#\e[31m
-# \e[0m
+#\e[31m to enable colour code 31
+# \e[0m To disable the colour code
+# -e enable esc sequence \e is one esc sequence
+# "" quotes are mandatory if we use esc seq
+
