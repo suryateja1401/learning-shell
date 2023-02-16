031e1f3377832da8206d2ba508be6d56aab735b7
TEJA <suryateja1401@gmail.com>
2023-02-16 07:21:47 +0530

    conditions

--- print.sh
+++ print.sh
@@ -6,9 +6,9 @@ echo -e "\e[COLm MESSAGE  \e[0m"
 # \e[0m To disable the colour code
 # -e enable esc sequence \e is one esc sequence
 # "" quotes are mandatory if we use esc seq
-#31
-#32
-#33
-#34
-#35
-
+#31 RED
+#32 GREEN
+#33 YELLOW
+#34 BLUE
+#35 MANGETA
+# 36 CYAN
